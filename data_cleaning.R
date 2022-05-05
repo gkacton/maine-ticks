84b4d0ff610599b480354a029a9dd9b488ef965f
Grace Acton <gacton@bates.edu>
2022-05-05 19:10:41 +0000

    Add Matt's data cleaning script for incidence data

--- data_cleaning.R
+++ data_cleaning.R
@@ -19,3 +19,29 @@ incidence <- read_csv("data/ticks/maine_tracking_network_incidence.csv")
 rates <- read_csv("data/ticks/maine_tracking_network_rate.csv")
 prevalence <- read_csv("data/ticks/umaine_tickborne_prevalence_town.csv")
 
+# Cleaning Incidence Data -------------------------------------------------
+  ## From Matt's data cleaning script 
+
+tick_numbers <- incidence %>% 
+  mutate(Lyme_Label = Lyme) %>%
+  mutate(Lyme_Label = str_replace(Lyme_Label, "NR", "Not Releasable")) %>%
+  mutate(Lyme = str_replace(Lyme, "<6", "6")) %>%
+  mutate(Lyme = str_replace(Lyme, "6-10", "8")) %>%
+  mutate(Lyme = str_replace(Lyme, "11-15", "13")) %>%
+  mutate(Babesiosis_Label = Babesiosis) %>%
+  mutate(Babesiosis_Label = str_replace(Babesiosis_Label, "NR", "Not Releasable")) %>%
+  mutate(Babesiosis = str_replace(Babesiosis, "<6", "6")) %>%
+  mutate(Babesiosis = str_replace(Babesiosis, "6-10", "8")) %>%
+  mutate(Babesiosis = str_replace(Babesiosis, "11-15", "13")) %>%
+  mutate(Anaplasmosis_Label = Anaplasmosis) %>%
+  mutate(Anaplasmosis_Label = str_replace(Anaplasmosis_Label, "NR", "Not Releasable")) %>%
+  mutate(Anaplasmosis = str_replace(Anaplasmosis, "<6", "6")) %>%
+  mutate(Anaplasmosis = str_replace(Anaplasmosis, "6-10", "8")) %>%
+  mutate(Anaplasmosis = str_replace(Anaplasmosis, "11-15", "13")) %>% 
+  mutate(lyme = as.numeric(Lyme),
+         babesiosis = as.numeric(Babesiosis),
+         anaplasmosis = as.numeric(Anaplasmosis)) %>% 
+  select(Location, lyme, babesiosis, anaplasmosis, Population) 
+
+
+
